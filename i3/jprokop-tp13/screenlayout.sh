ab1635cd107faa2cae8e23f3d1c29c3fb4e4bfb4
Jiří Prokop <jprokop@synaptiko.cz>
2017-04-18 20:22:21 +0200

    Typo in screenlayout.sh fixed

--- i3/jprokop-tp13/screenlayout.sh
+++ i3/jprokop-tp13/screenlayout.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if xrandr | grep "HDMI1 disconnected"; then
 	xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
-then
+else
 	xrandr --output HDMI1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal
 fi
